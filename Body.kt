ea6b7bc48a2bfaa39e8d7f955d0e07d7a2b4994a
duying <mengqidlufei8932@gmial.com>
2020-05-04 14:48:11 +0800

    [feature-0.0.1 Body] 新增: 添加基础代码逻辑

--- Body.kt
+++ Body.kt
@@ -1,8 +1,16 @@
 public class Body {
 
     fun main(array: StringArray) {
-        
-
+        var value = sum(1, 2, 3, 4, 5, 6)
+        println(value)
     } 
+
+    fun sum(vararg args: Int): Int{
+        var sum
+        args.foreach {
+            sum += it
+        }
+        return sum
+    }
 
 }
